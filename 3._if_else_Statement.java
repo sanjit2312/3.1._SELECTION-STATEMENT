baed864b0d939e6e62bf8cf85422c0a4edbefc89
Sanjit Kumar Singh <62151811+sanjit2312@users.noreply.github.com>
2020-07-16 20:35:36 +0530

    Add files via upload

---
+++ 3._if_else_Statement.java
@@ -0,0 +1,16 @@
+package selectionStatement;
+
+public class if_else_Statement
+{
+
+	public static void main(String[] args)
+	{
+		
+		if(5>5)
+			System.out.println("If Statement");
+		else
+			System.out.println("Else Statement");
+		
+	}
+
+}
