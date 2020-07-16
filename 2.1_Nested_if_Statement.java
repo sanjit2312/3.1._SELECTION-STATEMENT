967886a2ac09074dac852ddc0c7d401d2cca91d7
Sanjit Kumar Singh <62151811+sanjit2312@users.noreply.github.com>
2020-07-16 20:34:33 +0530

    Add files via upload

---
+++ 2.1_Nested_if_Statement.java
@@ -0,0 +1,20 @@
+package selectionStatement;
+
+public class Nested_if_Statement
+{
+
+	public static void main(String[] args)
+	{
+		
+		if(5>6)
+		{
+			if(3<4)
+			{
+				System.out.println("Sanjit");
+			}
+		}
+
+		System.out.println("Program End !!!");
+	}
+
+}
