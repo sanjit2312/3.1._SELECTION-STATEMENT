bde36cfe6f5b9bc902178b6a4d9c8479bb28c6f0
Sanjit Kumar Singh <62151811+sanjit2312@users.noreply.github.com>
2020-07-16 20:36:40 +0530

    Add files via upload

---
+++ 5.1_else_if_Statement.java
@@ -0,0 +1,19 @@
+package selectionStatement;
+
+public class else_if_Statement
+{
+
+	public static void main(String[] args)
+	{
+		if(3<4)
+		{
+			System.out.println("4 is greater than 3");
+		}
+		else if(4<3)
+		{
+			System.out.println("3 is greater than 4");
+		}
+	
+	}
+
+}
